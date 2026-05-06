14e2b5e74fb03a9114109fadaeb843f95d558f0d
Zaq <zaq@lengmedia.com>
2026-05-06 11:39:55 +0100

    fix: correct vercel adapter import path (@astrojs/vercel/serverless)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://www.aylaproperty.com',
